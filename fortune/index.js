487f4ebc389da14c4649aee749ff3a41727a33bc
Adam Robson <adamray312@gmail.com>
2023-11-12 15:33:39 -0800

    add the links, and the stylesheets and the root html docs for game routes with assets

---
+++ fortune/index.js
@@ -0,0 +1,66 @@
+const questionInput = document.getElementById('question');
+const questionBtn = document.querySelector('.question-btn');
+const showQuestion = document.getElementById('show-question');
+const answerSection = document.getElementById('answer-section');
+const answer = document.querySelector('.answer');
+const resetSection = document.querySelector('.reset-section')
+const resetBtn = document.getElementById('reset-btn');
+
+const answers = [
+  'i suggest that you do not go for it! instead, look for a hobby in which you find purpose.',
+  'because i can not confirm beyond a shadow of a doubt, i will not. try again in an hour!',
+  'keep trying every 15 minutes!',
+  'i am hungry, come back in 20 minutes',
+  'who really knows?',
+  'it could come true!',
+  'i forsee it taking forever to happen. change your focus.',
+  'i dont have an answer for you!',
+  'now is not a good time, come back in 50 minutes'
+];
+
+questionBtn.addEventListener('click', () => {
+  const question = questionInput.value;
+  if (!question || question === undefined) {
+    questionBtn.disabled = true;
+    resetSection.classList.remove('hidden');
+    showQuestion.classList.add('error');
+    showQuestion.textContent = 'Please enter a question!';
+  } else {
+    questionBtn.disabled = true;
+    showQuestion.classList.remove('error');
+    copyQuestion();
+    toggleVisibility();
+    random();
+  }
+});
+
+const copyQuestion = () => {
+  const question = questionInput.value;
+  showQuestion.textContent = `Your question is: "${question}"`;
+  questionInput.value = '';
+};
+
+const toggleVisibility = () => {
+  answerSection.classList.remove('hidden');
+  resetSection.classList.remove('hidden');
+};
+
+const getRandomAnswer = (arr) => {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  const item = arr[randomIndex];
+  return item;
+};
+
+const random = () => {
+  const randomAnswer = getRandomAnswer(answers);
+  return answer.textContent = `My answer is: "${randomAnswer}"`;
+};
+
+resetBtn.addEventListener('click', () => {
+  showQuestion.textContent = '';
+  answer.textContent = '';
+  questionInput.value = '';
+  answerSection.classList.add('hidden');
+  resetSection.classList.add('hidden');
+  questionBtn.disabled = false;
+});
---
+++ shell/index.js
@@ -0,0 +1,100 @@
+// IMPORT DOM ELS
+const buttonOne = document.getElementById('button-one');
+const buttonTwo = document.getElementById('button-two');
+const buttonThree = document.getElementById('button-three');
+const cupOneEl = document.getElementById('cup-one');
+const cupTwoEl = document.getElementById('cup-two');
+const cupThreeEl = document.getElementById('cup-three');
+const resetButtonEl = document.getElementById('reset-button');
+const startOverBtn = document.getElementById('start-over');
+let correctGuesses = document.getElementById('correct-guesses');
+let totalGuesses = document.getElementById('total-guesses');
+let incorrectGuesses = document.getElementById('incorrect-guesses');
+
+// STATE
+const cupImages = [cupOneEl, cupTwoEl, cupThreeEl];
+
+let correctGuess = 0;
+let incorrectGuess = 0;
+let totalGuess = 0;
+
+// EVENT LISTENERS
+buttonOne.addEventListener('click', () => {
+  handleGuess(0, getRandomItem());
+}),
+
+buttonTwo.addEventListener('click', () => {
+  handleGuess(1, getRandomItem());
+}),
+
+buttonThree.addEventListener('click', () => {
+  handleGuess(2, getRandomItem());
+}),
+
+resetButtonEl.addEventListener('click', () => {
+  cupOneEl.src = '../assets/graycup.png';
+  cupTwoEl.src = '../assets/graycup.png';
+  cupThreeEl.src = '../assets/graycup.png';
+  enableCupButtons();
+});
+
+startOverBtn.addEventListener('click', () => {
+  correctGuess = 0;
+  incorrectGuess = 0;
+  totalGuess = 0;
+  cupOneEl.src = '../assets/graycup.png';
+  cupTwoEl.src = '../assets/graycup.png';
+  cupThreeEl.src = '../assets/graycup.png';
+  enableCupButtons();
+  correctGuesses.textContent = 0;
+  incorrectGuesses.textContent = 0;
+  totalGuesses.textContent = 0;
+});
+
+// FUNCTIONS
+
+function resetCups() {
+  cupOneEl.src = '../assets/graycup.png';
+  cupTwoEl.src = '../assets/graycup.png';
+  cupThreeEl.src = '../assets/graycup.png';
+}
+
+function getRandomItem() {
+  const correctSpot = Math.floor(Math.random() * 3);
+  return correctSpot;
+}
+
+function handleGuess(userGuess, correctSpot) {
+  // resets the style
+  resetCups();
+  getRandomItem();
+  if (userGuess === correctSpot) {
+    correctGuess++;
+    totalGuess++;
+    correctGuesses.textContent = correctGuess;
+    totalGuesses.textContent = totalGuess;
+    incorrectGuesses.textContent = incorrectGuess;
+  } else {
+    incorrectGuess++;
+    totalGuess++;
+    correctGuesses.textContent = correctGuess;
+    incorrectGuesses.textContent = incorrectGuess;
+    totalGuesses.textContent = totalGuess;
+  }
+  cupImages[correctSpot].src = '../assets/graycupguy.png';
+  disableCupButtons();
+}
+
+
+
+function disableCupButtons() {
+  buttonOne.disabled = true;
+  buttonTwo.disabled = true;
+  buttonThree.disabled = true;
+}
+
+function enableCupButtons() {
+  buttonOne.disabled = false;
+  buttonTwo.disabled = false;
+  buttonThree.disabled = false;
+}
